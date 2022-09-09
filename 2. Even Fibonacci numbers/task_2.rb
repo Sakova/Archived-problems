e668a8e3662ec8dbb58af12696fccd830dd01f34
Alexander Korchyk <aliaksandr.korchyk@gmail.com>
2022-09-09 14:04:12 +0300

    fix: put the number in a constant

--- 2. Even Fibonacci numbers/task_2.rb
+++ 2. Even Fibonacci numbers/task_2.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-def fibonacci(limit = 4_000_000, num1 = 1, num2 = 1)
-  return 0 if limit > 4_000_000 || limit <= 0
+LIMIT = 4_000_000
+
+def fibonacci(limit = LIMIT, num1 = 1, num2 = 1)
+  return 0 if limit > LIMIT || limit <= 0
 
   num3 = num1 + num2
   return 0 if num3 > limit
