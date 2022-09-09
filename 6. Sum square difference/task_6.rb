30358553feba863f3c0ff7f8aae860d1ead08822
Alexander Korchyk <aliaksandr.korchyk@gmail.com>
2022-09-09 19:02:08 +0300

    fix: change method in task 6

--- 6. Sum square difference/task_6.rb
+++ 6. Sum square difference/task_6.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 def difference(num)
-  sum = 0
-  square = 0
-
-  (1..num).each do |i|
-    sum += i**2
-    square += i
-  end
-
-  (square**2) - sum
+  range = (1..num)
+  (range.reduce(:+)**2) - (range.reduce { |sum, num| sum + (num**2) })
 end
