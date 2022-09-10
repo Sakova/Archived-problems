8c5b98d91a509883cc48d25950fe9411f9870666
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-09-10 20:35:03 +0300

    fix: change rspec test for task 8

--- spec/task_8_spec.rb
+++ spec/task_8_spec.rb
@@ -5,7 +5,7 @@ require_relative '../8. Largest product in a series/task_8'
 RSpec.describe '#largest_product' do
   context 'with correct data' do
     it 'returns the largest palindrome made from the product of two input-digit numbers' do
-      expect(largest_product(10**1000)).to eq(1)
+      expect(largest_product(25_764_138_974_651_325_789, 2)).to eq(72)
     end
   end
 
